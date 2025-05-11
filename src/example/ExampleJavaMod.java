11988096a5e9f54a901740207672bc7daf0f110b
m1cxzfw3q <mcxzfwq@outlook.com>
2025-05-11 09:40:40 +0800

    test

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -1,36 +1,50 @@
 package example;
 
-import arc.*;
-import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
-import mindustry.game.EventType.*;
+import arc.math.geom.Rect;
 import mindustry.gen.*;
 import mindustry.mod.*;
-import mindustry.ui.dialogs.*;
+import mindustry.type.UnitType;
+import mindustry.type.unit.TankUnitType;
 
-public class ExampleJavaMod extends Mod{
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
-    public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
-
-        //listen for game load event
-        Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
-        });
-    }
+public class ExampleJavaMod extends Mod {
+    public static @EntityDef({Unitc.class, Tankc.class}) UnitType baidunwang;
 
     @Override
     public void loadContent(){
-        Log.info("Loading some example content.");
+        baidunwang = new TankUnitType("baidunwang") {{
+            hitSize = 10f * 8;
+            treadPullOffset = 5;
+            speed = 3f;
+            health = 50000;
+            armor = 40f;
+            crushDamage = 200f;
+            rotateSpeed = 2f;
+
+            float xo = 231f/2f, yo = 231f/2f;
+            treadRects = new Rect[]{new Rect(27 - xo, 152 - yo, 56, 73), new Rect(24 - xo, 51 - 9 - yo, 29, 17), new Rect(59 - xo, 18 - 9 - yo, 39, 19)};
+        }};
     }
 
+
+    /** Indicates an entity definition. */
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface EntityDef{
+        /** List of component interfaces */
+        Class[] value();
+        /** Whether the class is final */
+        boolean isFinal() default true;
+        /** If true, entities are recycled. */
+        boolean pooled() default false;
+        /** Whether to serialize (makes the serialize method return this value).
+         * If true, this entity is automatically put into save files.
+         * If false, no serialization code is generated at all. */
+        boolean serialize() default true;
+        /** Whether to generate IO code. This is for advanced usage only. */
+        boolean genio() default true;
+        /** Whether I made a massive mistake by merging two different class branches */
+        boolean legacy() default false;
+    }
 }
