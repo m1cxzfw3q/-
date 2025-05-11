6a425adb8474c86ca21bbe1661309db349910a2f
m1cxzfw3q <mcxzfwq@outlook.com>
2025-05-11 10:33:44 +0800

    test

--- src/baidunwang/ExampleJavaMod.java
+++ src/baidunwang/ExampleJavaMod.java
@@ -10,18 +10,20 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 public class ExampleJavaMod extends Mod {
-    public static @EntityDef({Unitc.class, Tankc.class}) UnitType baidunwang;
+    public static @EntityDef({Unitc.class, Tankc.class}) UnitType baidunwang1;
 
     @Override
     public void loadContent(){
-        baidunwang = new TankUnitType("baidunwang") {{
+        baidunwang1 = new TankUnitType("baidunwang1") {{
+            flying = false;
             hitSize = 10f * 8;
-            treadPullOffset = 5;
+            treadPullOffset = 1;
             speed = 3f;
             health = 50000;
             armor = 40f;
             crushDamage = 200f;
             rotateSpeed = 2f;
+            constructor = UnitEntity::create;
 
             float xo = 231f/2f, yo = 231f/2f;
             treadRects = new Rect[]{new Rect(27 - xo, 152 - yo, 56, 73), new Rect(24 - xo, 51 - 9 - yo, 29, 17), new Rect(59 - xo, 18 - 9 - yo, 39, 19)};
